396c544a2944930a1dc666bface35eb5103e0fa9
Maycon Douglas <mayconcti98@gmail.com>
2019-08-15 07:07:11 -0300

    enviado script do banco

---
+++ projeto_permissoes.sql
@@ -0,0 +1,88 @@
+﻿
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Banco de dados: `projeto_permissoes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `documentos`
+--
+
+CREATE TABLE `documentos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `documentos`
+--
+
+INSERT INTO `documentos` (`id`, `titulo`) VALUES
+(1, 'Alvará de Fulano'),
+(2, 'Cpf de cicrano'),
+(3, 'Recibo de carro X');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `permissoes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+--Senha = 123 em md5(202cb962ac59075b964b07152d234b70)
+INSERT INTO `usuarios` (`id`, `email`, `senha`, `nome`, `permissoes`) VALUES
+(1, fulano@gmail.com', '202cb962ac59075b964b07152d234b70', 'Fulano', 'DELL,ADD,SECRET,EDIT');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `documentos`
+--
+ALTER TABLE `documentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `documentos`
+--
+ALTER TABLE `documentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
